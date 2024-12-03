d35652eaf245c464db0440863b95f352ea394444
sourav819 <biswassourav819@gmail.com>
2024-12-03 23:46:56 +0530

    add env files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYmEzZTc4NGU5MWM2MzBkYWQ4ZDNmMDkyNGYyY2EyZiIsIm5iZiI6MTczMjQ2MTE1Mi42OTYzMDQzLCJzdWIiOiI2NzQzNDE5YTRkNTBjY2Q3ZGE0OGFmZmIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._RnpATSfstfUvdSFmaqYQ4KSx-UQgtC_DXc22T0K2CY",
+    Authorization: "Bearer" + process.env.REACT_APP_API_KEY,
   },
 };
 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCe-l1q8EYz4ORXkDKvyigp7yYMsrqQ8ic",
-  authDomain: "streamwise-f2996.firebaseapp.com",
-  projectId: "streamwise-f2996",
-  storageBucket: "streamwise-f2996.firebasestorage.app",
-  messagingSenderId: "987356489142",
-  appId: "1:987356489142:web:2d10e76bbf42a57e47cd17",
-  measurementId: "G-FRGWKC126J",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
